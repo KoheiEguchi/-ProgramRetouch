9f8b0cb33e5d609d4fa740e95ac5f6064b419692
KoheiEguchi <ytictnaidar2642@gmail.com>
2018-05-31 16:41:34 +0900

    課題1-3まで完了

--- EC/src/ec/BuyConfirm.java
+++ EC/src/ec/BuyConfirm.java
@@ -39,6 +39,7 @@ public class BuyConfirm extends HttpServlet {
 			BuyDataBeans bdb = new BuyDataBeans();
 			bdb.setUserId((int) session.getAttribute("userId"));
 			bdb.setTotalPrice(totalPrice);
+			bdb.setDelivertMethodId(userSelectDMB.getId());
 			bdb.setDeliveryMethodName(userSelectDMB.getName());
 			bdb.setDeliveryMethodPrice(userSelectDMB.getPrice());
 
